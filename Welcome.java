f983357462bc946db0015d29eb262d2ab3d7bffd
tranquocsang23715111 <23715111.sang@student.iuh.edu.vn>
2024-08-15 11:24:10 +0700

    Add files via upload

---
+++ Welcome.java
@@ -0,0 +1,6 @@
+public class Welcome{
+    public static void main(String[] ages){
+        System.out.println("Hello Trần Quốc Sang!");
+        System.out.println("Welcome to the DHKHDL19B");
+    }
+}
